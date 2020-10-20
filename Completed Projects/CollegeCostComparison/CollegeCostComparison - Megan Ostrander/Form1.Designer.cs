378f0effecf34e47f03a50b9f2c6f44937624436
Megan Ostrander <mostrander181@gmail.com>
2020-10-20 13:42:41 -0400

    Add files via upload

---
+++ Completed Projects/CollegeCostComparison/CollegeCostComparison - Megan Ostrander/Form1.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace CollegeCostComparison___Megan_Ostrander
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.label4 = new System.Windows.Forms.Label();
+         this.label5 = new System.Windows.Forms.Label();
+         this.label6 = new System.Windows.Forms.Label();
+         this.label7 = new System.Windows.Forms.Label();
+         this.college1_Name = new System.Windows.Forms.TextBox();
+         this.college2_Name = new System.Windows.Forms.TextBox();
+         this.college2_State = new System.Windows.Forms.TextBox();
+         this.college1_State = new System.Windows.Forms.TextBox();
+         this.college2_Trips = new System.Windows.Forms.TextBox();
+         this.college1_Trips = new System.Windows.Forms.TextBox();
+         this.college2_Distance = new System.Windows.Forms.TextBox();
+         this.college1_Distance = new System.Windows.Forms.TextBox();
+         this.college2_Fee = new System.Windows.Forms.TextBox();
+         this.college1_Fee = new System.Windows.Forms.TextBox();
+         this.college2_Tuition = new System.Windows.Forms.TextBox();
+         this.college1_Tuition = new System.Windows.Forms.TextBox();
+         this.college2_RoomBoard = new System.Windows.Forms.TextBox();
+         this.college1_RoomBoard = new System.Windows.Forms.TextBox();
+         this.label8 = new System.Windows.Forms.Label();
+         this.label9 = new System.Windows.Forms.Label();
+         this.label10 = new System.Windows.Forms.Label();
+         this.calculate_Button = new System.Windows.Forms.Button();
+         this.label11 = new System.Windows.Forms.Label();
+         this.label12 = new System.Windows.Forms.Label();
+         this.college1_Result1yr = new System.Windows.Forms.Label();
+         this.college2_Result1yr = new System.Windows.Forms.Label();
+         this.college1_Result4yr = new System.Windows.Forms.Label();
+         this.college2_Result4yr = new System.Windows.Forms.Label();
+         this.clearButton = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(29, 69);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(108, 20);
+         this.label1.TabIndex = 0;
+         this.label1.Text = "College Name";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(29, 104);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(220, 20);
+         this.label2.TabIndex = 1;
+         this.label2.Text = "State where college is located";
+         // 
+         // label3
+         // 
+         this.label3.Location = new System.Drawing.Point(29, 147);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(293, 77);
+         this.label3.TabIndex = 2;
+         this.label3.Text = "Number of one-way trips you estimate you will drive in a year from your residence" +
+    " to the college";
+         // 
+         // label4
+         // 
+         this.label4.Location = new System.Drawing.Point(29, 226);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(262, 59);
+         this.label4.TabIndex = 3;
+         this.label4.Text = "One-way distance, in miles, from your residence to the college";
+         // 
+         // label5
+         // 
+         this.label5.AutoSize = true;
+         this.label5.Location = new System.Drawing.Point(29, 297);
+         this.label5.Name = "label5";
+         this.label5.Size = new System.Drawing.Size(293, 20);
+         this.label5.TabIndex = 4;
+         this.label5.Text = "College’s One-time Application Fee Cost";
+         // 
+         // label6
+         // 
+         this.label6.AutoSize = true;
+         this.label6.Location = new System.Drawing.Point(29, 339);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(215, 20);
+         this.label6.TabIndex = 5;
+         this.label6.Text = "College’s Annual Tuition Cost";
+         // 
+         // label7
+         // 
+         this.label7.AutoSize = true;
+         this.label7.Location = new System.Drawing.Point(29, 384);
+         this.label7.Name = "label7";
+         this.label7.Size = new System.Drawing.Size(262, 20);
+         this.label7.TabIndex = 6;
+         this.label7.Text = "College’s Annual Room & Board Cost";
+         // 
+         // college1_Name
+         // 
+         this.college1_Name.Location = new System.Drawing.Point(343, 63);
+         this.college1_Name.Name = "college1_Name";
+         this.college1_Name.Size = new System.Drawing.Size(134, 26);
+         this.college1_Name.TabIndex = 7;
+         // 
+         // college2_Name
+         // 
+         this.college2_Name.Location = new System.Drawing.Point(514, 63);
+         this.college2_Name.Name = "college2_Name";
+         this.college2_Name.Size = new System.Drawing.Size(134, 26);
+         this.college2_Name.TabIndex = 8;
+         // 
+         // college2_State
+         // 
+         this.college2_State.Location = new System.Drawing.Point(514, 104);
+         this.college2_State.Name = "college2_State";
+         this.college2_State.Size = new System.Drawing.Size(134, 26);
+         this.college2_State.TabIndex = 10;
+         // 
+         // college1_State
+         // 
+         this.college1_State.Location = new System.Drawing.Point(343, 104);
+         this.college1_State.Name = "college1_State";
+         this.college1_State.Size = new System.Drawing.Size(134, 26);
+         this.college1_State.TabIndex = 9;
+         // 
+         // college2_Trips
+         // 
+         this.college2_Trips.Location = new System.Drawing.Point(514, 147);
+         this.college2_Trips.Name = "college2_Trips";
+         this.college2_Trips.Size = new System.Drawing.Size(134, 26);
+         this.college2_Trips.TabIndex = 12;
+         // 
+         // college1_Trips
+         // 
+         this.college1_Trips.Location = new System.Drawing.Point(343, 147);
+         this.college1_Trips.Name = "college1_Trips";
+         this.college1_Trips.Size = new System.Drawing.Size(134, 26);
+         this.college1_Trips.TabIndex = 11;
+         // 
+         // college2_Distance
+         // 
+         this.college2_Distance.Location = new System.Drawing.Point(514, 226);
+         this.college2_Distance.Name = "college2_Distance";
+         this.college2_Distance.Size = new System.Drawing.Size(134, 26);
+         this.college2_Distance.TabIndex = 14;
+         // 
+         // college1_Distance
+         // 
+         this.college1_Distance.Location = new System.Drawing.Point(343, 226);
+         this.college1_Distance.Name = "college1_Distance";
+         this.college1_Distance.Size = new System.Drawing.Size(134, 26);
+         this.college1_Distance.TabIndex = 13;
+         // 
+         // college2_Fee
+         // 
+         this.college2_Fee.Location = new System.Drawing.Point(514, 297);
+         this.college2_Fee.Name = "college2_Fee";
+         this.college2_Fee.Size = new System.Drawing.Size(134, 26);
+         this.college2_Fee.TabIndex = 16;
+         // 
+         // college1_Fee
+         // 
+         this.college1_Fee.Location = new System.Drawing.Point(343, 297);
+         this.college1_Fee.Name = "college1_Fee";
+         this.college1_Fee.Size = new System.Drawing.Size(134, 26);
+         this.college1_Fee.TabIndex = 15;
+         // 
+         // college2_Tuition
+         // 
+         this.college2_Tuition.Location = new System.Drawing.Point(514, 339);
+         this.college2_Tuition.Name = "college2_Tuition";
+         this.college2_Tuition.Size = new System.Drawing.Size(134, 26);
+         this.college2_Tuition.TabIndex = 18;
+         // 
+         // college1_Tuition
+         // 
+         this.college1_Tuition.Location = new System.Drawing.Point(343, 339);
+         this.college1_Tuition.Name = "college1_Tuition";
+         this.college1_Tuition.Size = new System.Drawing.Size(134, 26);
+         this.college1_Tuition.TabIndex = 17;
+         // 
+         // college2_RoomBoard
+         // 
+         this.college2_RoomBoard.Location = new System.Drawing.Point(514, 384);
+         this.college2_RoomBoard.Name = "college2_RoomBoard";
+         this.college2_RoomBoard.Size = new System.Drawing.Size(134, 26);
+         this.college2_RoomBoard.TabIndex = 20;
+         // 
+         // college1_RoomBoard
+         // 
+         this.college1_RoomBoard.Location = new System.Drawing.Point(343, 384);
+         this.college1_RoomBoard.Name = "college1_RoomBoard";
+         this.college1_RoomBoard.Size = new System.Drawing.Size(134, 26);
+         this.college1_RoomBoard.TabIndex = 19;
+         // 
+         // label8
+         // 
+         this.label8.AutoSize = true;
+         this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label8.Location = new System.Drawing.Point(361, 24);
+         this.label8.Name = "label8";
+         this.label8.Size = new System.Drawing.Size(95, 25);
+         this.label8.TabIndex = 21;
+         this.label8.Text = "College 1";
+         // 
+         // label9
+         // 
+         this.label9.AutoSize = true;
+         this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label9.Location = new System.Drawing.Point(532, 24);
+         this.label9.Name = "label9";
+         this.label9.Size = new System.Drawing.Size(95, 25);
+         this.label9.TabIndex = 22;
+         this.label9.Text = "College 2";
+         // 
+         // label10
+         // 
+         this.label10.AutoSize = true;
+         this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label10.Location = new System.Drawing.Point(29, 466);
+         this.label10.Name = "label10";
+         this.label10.Size = new System.Drawing.Size(131, 20);
+         this.label10.TabIndex = 23;
+         this.label10.Text = "CALCULATIONS";
+         // 
+         // calculate_Button
+         // 
+         this.calculate_Button.Location = new System.Drawing.Point(343, 453);
+         this.calculate_Button.Name = "calculate_Button";
+         this.calculate_Button.Size = new System.Drawing.Size(306, 33);
+         this.calculate_Button.TabIndex = 24;
+         this.calculate_Button.Text = "Calculate Totals for both Colleges";
+         this.calculate_Button.UseVisualStyleBackColor = true;
+         this.calculate_Button.Click += new System.EventHandler(this.calculate_Button_Click);
+         // 
+         // label11
+         // 
+         this.label11.AutoSize = true;
+         this.label11.Location = new System.Drawing.Point(29, 508);
+         this.label11.Name = "label11";
+         this.label11.Size = new System.Drawing.Size(257, 20);
+         this.label11.TabIndex = 25;
+         this.label11.Text = "Calculated 1 Year Total FUEL Cost";
+         // 
+         // label12
+         // 
+         this.label12.AutoSize = true;
+         this.label12.Location = new System.Drawing.Point(29, 586);
+         this.label12.Name = "label12";
+         this.label12.Size = new System.Drawing.Size(270, 20);
+         this.label12.TabIndex = 26;
+         this.label12.Text = "Calculated 4 Year Total of ALL Costs";
+         // 
+         // college1_Result1yr
+         // 
+         this.college1_Result1yr.AutoSize = true;
+         this.college1_Result1yr.Location = new System.Drawing.Point(362, 508);
+         this.college1_Result1yr.Name = "college1_Result1yr";
+         this.college1_Result1yr.Size = new System.Drawing.Size(0, 20);
+         this.college1_Result1yr.TabIndex = 27;
+         // 
+         // college2_Result1yr
+         // 
+         this.college2_Result1yr.AutoSize = true;
+         this.college2_Result1yr.Location = new System.Drawing.Point(531, 508);
+         this.college2_Result1yr.Name = "college2_Result1yr";
+         this.college2_Result1yr.Size = new System.Drawing.Size(0, 20);
+         this.college2_Result1yr.TabIndex = 28;
+         // 
+         // college1_Result4yr
+         // 
+         this.college1_Result4yr.AutoSize = true;
+         this.college1_Result4yr.Location = new System.Drawing.Point(360, 586);
+         this.college1_Result4yr.Name = "college1_Result4yr";
+         this.college1_Result4yr.Size = new System.Drawing.Size(0, 20);
+         this.college1_Result4yr.TabIndex = 29;
+         // 
+         // college2_Result4yr
+         // 
+         this.college2_Result4yr.AutoSize = true;
+         this.college2_Result4yr.BackColor = System.Drawing.SystemColors.Control;
+         this.college2_Result4yr.Location = new System.Drawing.Point(531, 586);
+         this.college2_Result4yr.Name = "college2_Result4yr";
+         this.college2_Result4yr.Size = new System.Drawing.Size(0, 20);
+         this.college2_Result4yr.TabIndex = 30;
+         // 
+         // clearButton
+         // 
+         this.clearButton.Location = new System.Drawing.Point(261, 633);
+         this.clearButton.Name = "clearButton";
+         this.clearButton.Size = new System.Drawing.Size(101, 33);
+         this.clearButton.TabIndex = 31;
+         this.clearButton.Text = "Clear";
+         this.clearButton.UseVisualStyleBackColor = true;
+         this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(677, 698);
+         this.Controls.Add(this.clearButton);
+         this.Controls.Add(this.college2_Result4yr);
+         this.Controls.Add(this.college1_Result4yr);
+         this.Controls.Add(this.college2_Result1yr);
+         this.Controls.Add(this.college1_Result1yr);
+         this.Controls.Add(this.label12);
+         this.Controls.Add(this.label11);
+         this.Controls.Add(this.calculate_Button);
+         this.Controls.Add(this.label10);
+         this.Controls.Add(this.label9);
+         this.Controls.Add(this.label8);
+         this.Controls.Add(this.college2_RoomBoard);
+         this.Controls.Add(this.college1_RoomBoard);
+         this.Controls.Add(this.college2_Tuition);
+         this.Controls.Add(this.college1_Tuition);
+         this.Controls.Add(this.college2_Fee);
+         this.Controls.Add(this.college1_Fee);
+         this.Controls.Add(this.college2_Distance);
+         this.Controls.Add(this.college1_Distance);
+         this.Controls.Add(this.college2_Trips);
+         this.Controls.Add(this.college1_Trips);
+         this.Controls.Add(this.college2_State);
+         this.Controls.Add(this.college1_State);
+         this.Controls.Add(this.college2_Name);
+         this.Controls.Add(this.college1_Name);
+         this.Controls.Add(this.label7);
+         this.Controls.Add(this.label6);
+         this.Controls.Add(this.label5);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label label5;
+      private System.Windows.Forms.Label label6;
+      private System.Windows.Forms.Label label7;
+      private System.Windows.Forms.TextBox college1_Name;
+      private System.Windows.Forms.TextBox college2_Name;
+      private System.Windows.Forms.TextBox college2_State;
+      private System.Windows.Forms.TextBox college1_State;
+      private System.Windows.Forms.TextBox college2_Trips;
+      private System.Windows.Forms.TextBox college1_Trips;
+      private System.Windows.Forms.TextBox college2_Distance;
+      private System.Windows.Forms.TextBox college1_Distance;
+      private System.Windows.Forms.TextBox college2_Fee;
+      private System.Windows.Forms.TextBox college1_Fee;
+      private System.Windows.Forms.TextBox college2_Tuition;
+      private System.Windows.Forms.TextBox college1_Tuition;
+      private System.Windows.Forms.TextBox college2_RoomBoard;
+      private System.Windows.Forms.TextBox college1_RoomBoard;
+      private System.Windows.Forms.Label label8;
+      private System.Windows.Forms.Label label9;
+      private System.Windows.Forms.Label label10;
+      private System.Windows.Forms.Button calculate_Button;
+      private System.Windows.Forms.Label label11;
+      private System.Windows.Forms.Label label12;
+      private System.Windows.Forms.Label college1_Result1yr;
+      private System.Windows.Forms.Label college2_Result1yr;
+      private System.Windows.Forms.Label college1_Result4yr;
+      private System.Windows.Forms.Label college2_Result4yr;
+      private System.Windows.Forms.Button clearButton;
+   }
+}
+
---
+++ Completed Projects/CollegeCostComparison/CollegeCostComparison - Megan Ostrander/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*
+ * Author: Megan Ostrander
+ * ID: 000635277
+ * Date: September 12, 2020
+ * Goal: To create a program that allows the user to compare the cost of attending one college
+ *       versus attending another college. It compares the cost of 2 colleges at a time. 
+ *       It takes into account the cost of Tuition, Housing, and cost of gas for 4 years, 
+ *       including the one time application fee to apply to the college.
+ *       I included a reset button so program can be used multiple times without closing.
+*/
+
+
+
+
+namespace CollegeCostComparison___Megan_Ostrander
+{
+   public partial class Form1 : Form
+   {
+      //Assuming $2.50 per gallon of fuel purchased.
+      private static decimal fuelCost = 2.50m;
+      private static int milesPerGallon = 25;
+
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+
+      private void calculate_Button_Click(object sender, EventArgs e)
+      {
+         //Reset the background collor for calculation results for program reuse.
+         college1_Result4yr.BackColor = Color.White;
+         college2_Result4yr.BackColor = Color.White;
+
+
+         //Calculate number of miles driven to colleges per year
+         //Using decimal because distance may not be a whole number, and will need to 
+         //calculate cost accurately later.
+         decimal college1_Miles = decimal.Parse(college1_Trips.Text) * decimal.Parse(college1_Distance.Text);
+         decimal college2_Miles = decimal.Parse(college2_Trips.Text) * decimal.Parse(college2_Distance.Text);
+
+         //Calculate number of gallons used travelling to colleged
+         //Number of miles per gallon assumed to be 25.
+         decimal college1_Gallons = college1_Miles / milesPerGallon;
+         decimal college2_Gallons = college2_Miles / milesPerGallon;
+
+         //Calculate cost of gas for each college.
+         decimal college1_GasCost = college1_Gallons * fuelCost;
+         decimal college2_GasCost = college2_Gallons * fuelCost;
+
+         //Test: Display gas / year cost in college 1 year total labels to verify calculations
+         //Realized I am supposed to show ONLY fuel cost for 1 year, so I am using this code in production.
+         college1_Result1yr.Text = college1_GasCost.ToString("c");
+         college2_Result1yr.Text = college2_GasCost.ToString("c");
+
+
+
+         //Calculate total cost of attending colleges for 1 year.
+         //I removed the following lines because I realized I did not need them. Kept for reference.
+
+         //decimal college1_TotalCost1yr = college1_GasCost + decimal.Parse(college1_Fee.Text) + 
+         //            decimal.Parse(college1_RoomBoard.Text) + decimal.Parse(college1_Tuition.Text);
+
+         //decimal college2_TotalCost1yr = college2_GasCost + decimal.Parse(college2_Fee.Text) +
+         //            decimal.Parse(college2_RoomBoard.Text) + decimal.Parse(college2_Tuition.Text);
+
+
+         //Display the 1 year calculation results. See above as to why this is commented out.
+         //college1_Result1yr.Text = college1_TotalCost1yr.ToString("C");
+         //college2_Result1yr.Text = college2_TotalCost1yr.ToString("C");
+
+
+
+         //Calculate total cost of attending colleges for 4 years
+         decimal college1_TotalCost4yr = 4 * ( college1_GasCost + decimal.Parse(college1_RoomBoard.Text) 
+                     + decimal.Parse(college1_Tuition.Text) ) + decimal.Parse(college1_Fee.Text);
+
+         decimal college2_TotalCost4yr = 4 * ( college2_GasCost + decimal.Parse(college2_RoomBoard.Text)
+                     + decimal.Parse(college2_Tuition.Text)) + decimal.Parse(college2_Fee.Text);
+
+
+         //Display the 4 year calculation results
+         college1_Result4yr.Text = college1_TotalCost4yr.ToString("C");
+         college2_Result4yr.Text = college2_TotalCost4yr.ToString("C");
+
+         if(college1_TotalCost4yr > college2_TotalCost4yr)
+         {
+            college1_Result4yr.BackColor = Color.Red;
+         }
+         else
+         {
+            college2_Result4yr.BackColor = Color.Red;
+         }
+
+      }
+
+
+      private void clearButton_Click(object sender, EventArgs e)
+      {
+         //This method clears the form data so it can be used multiple times.
+         //Clears any data in the College 1 text boxes.
+         college1_Name.Text = "";
+         college1_State.Text = "";
+         college1_Trips.Text = "";
+         college1_Distance.Text = "";
+         college1_Fee.Text = "";
+         college1_Tuition.Text = "";
+         college1_RoomBoard.Text = "";
+         college1_Result1yr.Text = "";
+         college1_Result4yr.Text = "";
+
+         //Clears any data in the College 2 text boxes
+         college2_Name.Text = "";
+         college2_State.Text = "";
+         college2_Trips.Text = "";
+         college2_Distance.Text = "";
+         college2_Fee.Text = "";
+         college2_Tuition.Text = "";
+         college2_RoomBoard.Text = "";
+         college2_Result1yr.Text = "";
+         college2_Result4yr.Text = "";
+
+         //Reset the calculation results background color.
+         college1_Result4yr.BackColor = Color.White;
+         college2_Result4yr.BackColor = Color.White;
+
+      }
+
+   }
+}
