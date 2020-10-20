ccec9b3446556fcd169d729c5d95efbf69b55343
Megan Ostrander <mostrander181@gmail.com>
2020-10-20 13:37:19 -0400

    Add files via upload

---
+++ Completed Projects/ShippingCalculator/ShippingCalculator - Megan Ostrander/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ShippingCalculator___Megan_Ostrander
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.weightBox = new System.Windows.Forms.TextBox();
+         this.shippingCodeBox = new System.Windows.Forms.TextBox();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.label4 = new System.Windows.Forms.Label();
+         this.label5 = new System.Windows.Forms.Label();
+         this.label6 = new System.Windows.Forms.Label();
+         this.weight_Label = new System.Windows.Forms.Label();
+         this.zone_Label = new System.Windows.Forms.Label();
+         this.shipping_Label = new System.Windows.Forms.Label();
+         this.netPound_Label = new System.Windows.Forms.Label();
+         this.cap_Label = new System.Windows.Forms.Label();
+         this.calculate_Button = new System.Windows.Forms.Button();
+         this.clear_Button = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // weightBox
+         // 
+         this.weightBox.Location = new System.Drawing.Point(160, 66);
+         this.weightBox.Name = "weightBox";
+         this.weightBox.Size = new System.Drawing.Size(102, 26);
+         this.weightBox.TabIndex = 0;
+         // 
+         // shippingCodeBox
+         // 
+         this.shippingCodeBox.Location = new System.Drawing.Point(160, 104);
+         this.shippingCodeBox.Name = "shippingCodeBox";
+         this.shippingCodeBox.Size = new System.Drawing.Size(102, 26);
+         this.shippingCodeBox.TabIndex = 1;
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(12, 69);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(125, 20);
+         this.label1.TabIndex = 2;
+         this.label1.Text = "Package Weight";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(25, 107);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(112, 20);
+         this.label2.TabIndex = 3;
+         this.label2.Text = "Shipping Zone";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(41, 188);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(108, 20);
+         this.label3.TabIndex = 4;
+         this.label3.Text = "Weight Cost : ";
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(54, 225);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(95, 20);
+         this.label4.TabIndex = 5;
+         this.label4.Text = "Zone Cost : ";
+         // 
+         // label5
+         // 
+         this.label5.AutoSize = true;
+         this.label5.Location = new System.Drawing.Point(29, 294);
+         this.label5.Name = "label5";
+         this.label5.Size = new System.Drawing.Size(120, 20);
+         this.label5.TabIndex = 6;
+         this.label5.Text = "Shipping Cost : ";
+         // 
+         // label6
+         // 
+         this.label6.AutoSize = true;
+         this.label6.Location = new System.Drawing.Point(25, 260);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(124, 20);
+         this.label6.TabIndex = 7;
+         this.label6.Text = "Net Per Pound : ";
+         // 
+         // weight_Label
+         // 
+         this.weight_Label.AutoSize = true;
+         this.weight_Label.Location = new System.Drawing.Point(156, 188);
+         this.weight_Label.Name = "weight_Label";
+         this.weight_Label.Size = new System.Drawing.Size(18, 20);
+         this.weight_Label.TabIndex = 8;
+         this.weight_Label.Text = "0";
+         // 
+         // zone_Label
+         // 
+         this.zone_Label.AutoSize = true;
+         this.zone_Label.Location = new System.Drawing.Point(156, 225);
+         this.zone_Label.Name = "zone_Label";
+         this.zone_Label.Size = new System.Drawing.Size(18, 20);
+         this.zone_Label.TabIndex = 9;
+         this.zone_Label.Text = "0";
+         // 
+         // shipping_Label
+         // 
+         this.shipping_Label.AutoSize = true;
+         this.shipping_Label.Location = new System.Drawing.Point(156, 294);
+         this.shipping_Label.Name = "shipping_Label";
+         this.shipping_Label.Size = new System.Drawing.Size(18, 20);
+         this.shipping_Label.TabIndex = 10;
+         this.shipping_Label.Text = "0";
+         // 
+         // netPound_Label
+         // 
+         this.netPound_Label.AutoSize = true;
+         this.netPound_Label.Location = new System.Drawing.Point(156, 260);
+         this.netPound_Label.Name = "netPound_Label";
+         this.netPound_Label.Size = new System.Drawing.Size(18, 20);
+         this.netPound_Label.TabIndex = 11;
+         this.netPound_Label.Text = "0";
+         // 
+         // cap_Label
+         // 
+         this.cap_Label.AutoSize = true;
+         this.cap_Label.Location = new System.Drawing.Point(230, 294);
+         this.cap_Label.Name = "cap_Label";
+         this.cap_Label.Size = new System.Drawing.Size(74, 20);
+         this.cap_Label.TabIndex = 12;
+         this.cap_Label.Text = "CAPPED";
+         this.cap_Label.Visible = false;
+         // 
+         // calculate_Button
+         // 
+         this.calculate_Button.Location = new System.Drawing.Point(105, 345);
+         this.calculate_Button.Name = "calculate_Button";
+         this.calculate_Button.Size = new System.Drawing.Size(129, 38);
+         this.calculate_Button.TabIndex = 13;
+         this.calculate_Button.Text = "Calculate";
+         this.calculate_Button.UseVisualStyleBackColor = true;
+         this.calculate_Button.Click += new System.EventHandler(this.calculate_Button_Click);
+         // 
+         // clear_Button
+         // 
+         this.clear_Button.Location = new System.Drawing.Point(105, 396);
+         this.clear_Button.Name = "clear_Button";
+         this.clear_Button.Size = new System.Drawing.Size(129, 38);
+         this.clear_Button.TabIndex = 14;
+         this.clear_Button.Text = "Clear";
+         this.clear_Button.UseVisualStyleBackColor = true;
+         this.clear_Button.Click += new System.EventHandler(this.clear_Button_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(330, 450);
+         this.Controls.Add(this.clear_Button);
+         this.Controls.Add(this.calculate_Button);
+         this.Controls.Add(this.cap_Label);
+         this.Controls.Add(this.netPound_Label);
+         this.Controls.Add(this.shipping_Label);
+         this.Controls.Add(this.zone_Label);
+         this.Controls.Add(this.weight_Label);
+         this.Controls.Add(this.label6);
+         this.Controls.Add(this.label5);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.shippingCodeBox);
+         this.Controls.Add(this.weightBox);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.TextBox weightBox;
+      private System.Windows.Forms.TextBox shippingCodeBox;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label label5;
+      private System.Windows.Forms.Label label6;
+      private System.Windows.Forms.Label weight_Label;
+      private System.Windows.Forms.Label zone_Label;
+      private System.Windows.Forms.Label shipping_Label;
+      private System.Windows.Forms.Label netPound_Label;
+      private System.Windows.Forms.Label cap_Label;
+      private System.Windows.Forms.Button calculate_Button;
+      private System.Windows.Forms.Button clear_Button;
+   }
+}
+
---
+++ Completed Projects/ShippingCalculator/ShippingCalculator - Megan Ostrander/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*
+ * Author: Megan Ostrander
+ * ID: 000635277
+ * Date: October 3, 2020
+ * Goal: To take user input for package weight and shipping zone code, and use that information to 
+ *       calculate the total shipping cost of the package, and cap the cost at $100 if it costs more than
+ *       that to ship the package. 
+*/
+
+
+namespace ShippingCalculator___Megan_Ostrander
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void calculate_Button_Click(object sender, EventArgs e)
+      {
+         //Variables created to hold data during program
+         String zone;                        //To hold user input for specified shipping zone.
+         double weight = 0;                  //To hold weight of package.
+         decimal weightCostTotal = 0.0m;     //Holds cost of package weight for shipping.
+         decimal shippingCost = 0.0m;        //Holds final shipping cost.
+
+         //Variables for zone shipping costs so they can be changed easily in one location.
+         decimal weightCost = 18.0m;         //Cost per pound to ship package
+         decimal zoneN_Cost = 27.0m;         //North zone.
+         decimal zoneS_Cost = 36.0m;         //South zone.
+         decimal zoneE_Cost = 45.0m;         //East zone.
+         decimal zoneW_Cost = 54.0m;         //West zone.
+
+         //First, validate that the weightBox field and shipping zone field is not 
+         //empty and contains valid numeric data
+         if (!String.IsNullOrEmpty(weightBox.Text) && !String.IsNullOrEmpty(shippingCodeBox.Text))
+         {
+            //Verify that weightBox field contains valid numeric data
+            if(double.TryParse(weightBox.Text, out weight))
+            {
+               //Verify that Shipping zone field only contains N, S, E, or W. 
+               if (shippingCodeBox.Text.Equals("N") || shippingCodeBox.Text.Equals("S") 
+                     || shippingCodeBox.Text.Equals("E") || shippingCodeBox.Text.Equals("W"))
+               {
+                  //Test to verify if statement ONLY registers N value in input.
+                  //Basically a test to verify that I have the right condition to test for.
+                  //MessageBox.Show("I am displaying because N is entered in the Shipping Code field!");
+                  //MessageBox.Show("I am displaying because the correct code is entered in the " +
+                  //   "Shipping Code field!");
+
+                  //Store the shipping zone code entered for later use.
+                  zone = shippingCodeBox.Text;
+
+                  //Calculate the cost to ship the package based only on weight
+                  weightCostTotal = (decimal)weight * weightCost;
+
+                  //Determine cost of shipping based on Zone code entered.
+                  //Added messageBoxes to test and make sure switch worked.
+                  //If greater than $100 statement must be switch for each case or as an additional method to work. 
+                  switch(zone)
+                  {
+                     case "N":
+                        //MessageBox.Show("Adding zone N charge!");
+                        shippingCost = weightCostTotal + zoneN_Cost;
+
+                        //MessageBox.Show("Shipping cost is now " + shippingCost.ToString("C"));
+
+                        //If shippingCost is above $100, cap it at $100.
+                        //if (shippingCost >= 100)
+                        //{
+                        //   //Test message to inform you when it is over 100, to verify it is processed right
+                        //   MessageBox.Show("Cost to ship package is over $100!");
+
+                        //   shippingCost = 100.00m;
+                        //   cap_Label.Visible = true;
+                        //}
+
+                        //Test of additional method created to hold above if statement.
+                        shippingCost = capped_Cost(shippingCost);  //It worked!
+
+                        //Calculate the net price per pound
+                        decimal netPoundPrice = shippingCost / (decimal)weight;
+
+                        //Display values on user screen
+                        weight_Label.Text = weightCost.ToString("C");
+                        netPound_Label.Text = netPoundPrice.ToString("C");
+                        shipping_Label.Text = shippingCost.ToString("C");
+                        zone_Label.Text = zoneN_Cost.ToString("C");
+
+                        return;
+
+                     case "S":
+                        shippingCost = weightCostTotal + zoneS_Cost;
+
+                        //MessageBox.Show("Shipping cost is now " + shippingCost.ToString("C"));
+                        shippingCost = capped_Cost(shippingCost);
+
+                        //Calculate the net price per pound
+                        netPoundPrice = shippingCost / (decimal)weight;
+
+                        //Display values on user screen
+                        weight_Label.Text = weightCost.ToString("C");
+                        netPound_Label.Text = netPoundPrice.ToString("C");
+                        shipping_Label.Text = shippingCost.ToString("C");
+                        zone_Label.Text = zoneS_Cost.ToString("C");
+
+                        return;
+
+                     case "E":
+                        shippingCost = weightCostTotal + zoneE_Cost;
+
+                        //MessageBox.Show("Shipping cost is now " + shippingCost.ToString("C"));
+                        shippingCost = capped_Cost(shippingCost);
+
+                        //Calculate the net price per pound
+                        netPoundPrice = shippingCost / (decimal)weight;
+
+                        //Display values on user screen
+                        weight_Label.Text = weightCost.ToString("C");
+                        netPound_Label.Text = netPoundPrice.ToString("C");
+                        shipping_Label.Text = shippingCost.ToString("C");
+                        zone_Label.Text = zoneE_Cost.ToString("C");
+
+                        return;
+                     case "W":
+                        shippingCost = weightCostTotal + zoneW_Cost;
+
+                        //MessageBox.Show("Shipping cost is now " + shippingCost.ToString("C"));
+                        shippingCost = capped_Cost(shippingCost);
+
+                        //Calculate the net price per pound
+                        netPoundPrice = shippingCost / (decimal)weight;
+
+                        //Display values on user screen
+                        weight_Label.Text = weightCost.ToString("C");
+                        netPound_Label.Text = netPoundPrice.ToString("C");
+                        shipping_Label.Text = shippingCost.ToString("C");
+                        zone_Label.Text = zoneW_Cost.ToString("C");
+
+                        return;
+                  }
+
+
+               }
+               else
+               {
+                  MessageBox.Show("Please enter one of the following values in the Shipping Code " +
+                     "field: N, S, E, or W.");
+               }
+            }
+            else
+            {
+               //If weightbox does not contain a value numeric input
+               MessageBox.Show("Please enter only a numeric value in the Package Weight field.");
+            }
+         }
+         else
+         {
+            //If the weightBox field is null or does not contain a value
+            if(String.IsNullOrEmpty(weightBox.Text))
+            {
+               MessageBox.Show("Please enter a numeric value in the package weight field.");
+            }
+
+            //If the shipping zone field is null or empty (does not contain a value).
+            if(String.IsNullOrEmpty(shippingCodeBox.Text))
+            {
+               MessageBox.Show("Please enter a value in the shipping zone field.");
+            }
+            
+         }
+      }
+
+
+      private decimal capped_Cost(decimal shippingCost)
+      {
+         if (shippingCost >= 100)
+         {
+            //Test message to inform you when it is over 100, to verify it is processed right
+            //MessageBox.Show("Cost to ship package is over $100!");
+
+            shippingCost = 100.00m;
+            cap_Label.Visible = true;
+         }
+
+         return shippingCost;
+      }
+
+
+      private void clear_Button_Click(object sender, EventArgs e)
+      {
+         //Clear the input fields
+         weightBox.Text = "";
+         shippingCodeBox.Text = "";
+
+         //Clear the display fields for values calculated.
+         weight_Label.Text = "";
+         netPound_Label.Text = "";
+         shipping_Label.Text = "";
+         zone_Label.Text = "";
+
+         //Hide the cap Label so it is not visible until new calculate value is about $100
+         cap_Label.Visible = false;
+
+      }
+   }
+}
